00a02a3f14828f1a0a30095f2cb07439ccca6ce0
Emily <episaygnane@me.com>
2022-07-26 16:02:15 -0700

    added event listeners, and handle guess function

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const buttonTwo = document.getElementById('button-one');
 const buttonThree = document.getElementById('button-one');
 const restButton = document.getElementById('reset-button');
 
-const flowerOne = document.getElementById('flower-one');
-const flowerTwo = document.getElementById('flower-two');
-const flowerThree = document.getElementById('flower-three');
+const flowerOne = document.getElementById('flowerOne-container');
+const flowerTwo = document.getElementById('flowerTwo-container');
+const flowerThree = document.getElementById('flowerThree-container');
 
 const totalEl = document.getElementById('total');
 const notFoundEl = document.getElementById('not-found');
@@ -17,11 +17,36 @@ const hidingPlaces = ['flower-one', 'flower-two', 'flower-three'];
 let foundGuesses = 0;
 let totalGuesses = 0;
 
-totalEl.textContent = totalGuesses;
-foundEl.textContent = foundGuesses;
-notFoundEl.textContent = totalGuesses - foundGuesses;
 
 // set event listeners 
+buttonOne.addEventListener('click', () => {
+
+  handleGuess(correctSpot, 'button-one');
+});
+
+buttonTwo.addEventListener('click', () => {
+
+  handleGuess(correctSpot, 'button-two');
+});
+
+buttonThree.addEventListener('click', () => {
+
+  handleGuess(correctSpot, 'button-three');
+});
   // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+  function handleGuess(correctSpot, userGuess) {
+    resetStyles();
+    totalGuesses++;
+    
+    const correctHidingPlaceEl = document.getElementById(`${correctSpot`}-container`);
+    
+    correctHidingPlaceEl.classList.add('fairy');
+    if (userGuess === correctSpot) {foundGuesses++;
+  }
+  
+  totalEl.textContent = totalGuesses;
+  foundEl.textContent = foundGuesses;
+  notFoundEl.textContent = totalGuesses - foundGuesses;
+}
+
+
