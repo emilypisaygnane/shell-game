89cf0e43a98af7bbbab855ea9b1b9f0f4ce8299f
Emily <episaygnane@me.com>
2022-07-26 15:43:16 -0700

    centering for CSS, and all the consts for JS

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // import functions and grab DOM elements
+const buttonOne = document.getElementById('button-one');
+const buttonTwo = document.getElementById('button-one');
+const buttonThree = document.getElementById('button-one');
+const restButton = document.getElementById('reset-button');
 
+const flowerOne = document.getElementById('flower-one');
+const flowerTwo = document.getElementById('flower-two');
+const flowerThree = document.getElementById('flower-three');
+
+const totalEl = document.getElementById('total');
+const notFoundEl = document.getElementById('not-found');
+const foundEl = document.getElementById('found');
 // let state
+const hidingPlaces = ['flower-one', 'flower-two', 'flower-three'];
+
+let foundGuesses = 0;
+let totalGuesses = 0;
+
+totalEl.textContent = totalGuesses;
+foundEl.textContent = foundGuesses;
+notFoundEl.textContent = totalGuesses - foundGuesses;
 
 // set event listeners 
   // get user input
