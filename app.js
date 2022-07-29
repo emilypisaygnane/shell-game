4db6cca0d7880e8010ec008acfcf848e973918fc
Emily <episaygnane@me.com>
2022-07-29 15:53:55 -0700

    fixed a lot of bugs, looks weird rn

--- app.js
+++ app.js
@@ -2,78 +2,61 @@
 const pinkButton = document.getElementById('pink-button');
 const blueButton = document.getElementById('blue-button');
 const purpleButton = document.getElementById('purple-button');
-const resetButton = document.getElementById('reset-button');
 
 const pinkFlower = document.getElementById('pink-flower');
 const blueFlower = document.getElementById('blue-flower');
 const purpleFlower = document.getElementById('purple-flower');
 
 const totalEl = document.getElementById('total');
-const notFoundEl = document.getElementById('not-found');
-const foundEl = document.getElementById('found');
+const hidingSpots = [pinkFlower, blueFlower, purpleFlower];
 
 // let state
-const hidingPlaces = ['pink-flower', 'blue-flower', 'purple-flower'];
+let found = 0;
+let notFound = 0;
+let total = 0;
 
-let foundGuesses = 0;
-let totalGuesses = 0;
-
-function getRandomItem(arr) {
-    const index = Math.floor(Math.random() * arr.length);
-
-    return arr[index];
+function resetImages() {
+    pinkFlower.src = './assets/pink-flower.jpg';
+    blueFlower.src = './assets/blue-flower.jpg';
+    purpleFlower.src = './assets/purple-flower.jpg';
 }
 
 // set event listeners 
 pinkButton.addEventListener('click', () => {
-    const correctSpot = getRandomItem(hidingPlaces);
+    resetImages();
+    total++;
+    const hidingSpot = Math.floor(Math.random() * 3);
+    const answer = hidingSpots[hidingSpot];
 
-    handleGuess(correctSpot, 'pink-button');
+    handleGuess(answer, 'pink');
 });
 
 blueButton.addEventListener('click', () => {
-    const correctSpot = getRandomItem(hidingPlaces);
-    
-    handleGuess(correctSpot, 'blue-button');
+    resetImages();
+    total++;
+    const hidingSpot = Math.floor(Math.random() * 3);
+    const answer = hidingSpots[hidingSpot];
+
+    handleGuess(answer, 'blue');
 });
 
 purpleButton.addEventListener('click', () => {
-    const correctSpot = getRandomItem(hidingPlaces);
-    
-    handleGuess(correctSpot, 'purple-button');
-});
+    resetImages();
+    total++;
+    const hidingSpot = Math.floor(Math.random() * 3);
+    const answer = hidingSpots[hidingSpot];
 
-resetButton.addEventListener('click', () => {
-    totalGuesses = 0;
-    foundGuesses = 0;
-    totalEl.textContent = totalGuesses;
-    foundEl.textContent = foundGuesses;
-    notFoundEl.textContent = totalGuesses - foundGuesses;
+    handleGuess(answer, 'purple');
 });
-
 
 function handleGuess(correctSpot, userGuess) {
-    resetStyles();
-    totalGuesses++;
+    correctSpot.src = './assets/silvermist-fairy.png';
 
-    const correctHidingPlaceEl = document.getElementById(`${correctSpot}-flower`);
-
-    correctHidingPlaceEl.classList.add('fairy');
-
-    if (userGuess === correctSpot) {
-        foundGuesses++;
+    if (correctSpot.id === userGuess) {
+        found++;
+    } else {
+        notFound++;
     }
-
-    totalEl.textContent = totalGuesses;
-    foundEl.textContent = foundGuesses;
-    notFoundEl.textContent = totalGuesses - foundGuesses;
+    totalEl.textContent = `You've found the fairy ${found} time, you didn't find the fairy ${notFound} times out of ${total} tries. Keep trying!`;
 }
-
-function resetStyles() {
-    pinkFlower.classList.remove('fairy');
-    blueFlower.classList.remove('fairy');
-    purpleFlower.classList.remove('fairy');
-}
-
-
 
