cbd89ba9b5afe5e59d6cbc2ea30fb71580b2b2d6
Emily <episaygnane@me.com>
2022-08-01 16:41:32 -0700

    added getRandomIndex, removed notFound, removed repeats in event listeners, and resized images

--- app.js
+++ app.js
@@ -8,13 +8,19 @@ const blueFlower = document.getElementById('blue-flower');
 const purpleFlower = document.getElementById('purple-flower');
 
 const totalEl = document.getElementById('total');
+const foundEl = document.getElementById('found');
+const notFoundEl = document.getElementById('not-found');                      
 const hidingSpots = [pinkFlower, blueFlower, purpleFlower];
 
 // let state
 let found = 0;
-let notFound = 0;
 let total = 0;
 
+function getRandomItem(arr) {
+    const index = Math.floor(Math.random() * 3);
+
+    return arr[index];
+}
 function resetImages() {
     pinkFlower.src = './assets/pink-flower.jpg';
     blueFlower.src = './assets/blue-flower.jpg';
@@ -23,41 +29,32 @@ function resetImages() {
 
 // set event listeners 
 pinkButton.addEventListener('click', () => {
-    resetImages();
-    total++;
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingSpots[hidingSpot];
+    const answer = getRandomItem(hidingSpots);
 
     handleGuess(answer, 'pink-flower');
 });
 
 blueButton.addEventListener('click', () => {
-    resetImages();
-    total++;
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingSpots[hidingSpot];
+    const answer = getRandomItem(hidingSpots);
 
     handleGuess(answer, 'blue-flower');
 });
 
 purpleButton.addEventListener('click', () => {
-    resetImages();
-    total++;
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingSpots[hidingSpot];
+    const answer = getRandomItem(hidingSpots);
 
     handleGuess(answer, 'purple-flower');
 });
 
 function handleGuess(correctSpot, userGuess) {
-
+    resetImages();
+    total++;
     correctSpot.src = './assets/silvermist-fairy.png';
 
     if (correctSpot.id === userGuess) {
         found++;
-    } else {
-        notFound++;
-    }
-    totalEl.textContent = `You've found the fairy ${found} time, you didn't find the fairy ${notFound} times out of ${total} tries. Keep trying!`;
+    } 
+    totalEl.textContent = total;
+    foundEl.textContent = found;
+    notFoundEl.textContent = total - found;
 }
-
